597e85a96b1af442abefc73e143a982dbfa9e95f
Mandeep-Singh18 <mandeepsingh36633@gmail.com>
2025-08-02 20:17:06 +0530

    start project

---
+++ apps/backend/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.post('/ai/training', (req, res) => {
+});
+
+app.post('/ai/generate', (req, res) => {
+
+});
+
+app.post('/pack/generate', (req, res) => {
+});
+
+app.get('/pack/bulk', (req, res) => {
+});
+
+app.get('/image', (req, res) => {
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
